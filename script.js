b196230a3a6d8b1a171bce631453d9e27b464a25
Kevin <kevin.lucassg@gmail.com>
2024-02-23 16:18:51 -0300

    Made the API request

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 const apiKey = "{API key}";
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=germany&appid={API key}&units=metric";
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bangalore";
+
+async function checkWeather() {
+  try {
+    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+    let data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = data.main.temp + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    console.log(data);
+  } catch {
+    alert("An error occurred in the API request");
+  }
+}
+
+checkWeather();
